4ff82537e261ecf9312d2f35e1bf6564035e4aa9
Codey Bloom <codey.bloom@protonmail.com>
2025-05-14 02:47:25 +1000

    finished the field logic and implemented type hinting everywhere

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+from typing import Type, TypeVar, cast
+
+# Because I want type hints everywhere the FieldEleement dataclass will be self referential. This helps with that.
+FE = TypeVar("FE", bound="FieldElement")
 
 
 @dataclass(frozen=True)
@@ -6,43 +10,50 @@ class FieldElement:
     num: int
     prime: int
 
+    """Below is all the requisite operations required to satisfy a field order. We use FE above to make it self-referring
+    with the type hints. We use cast when returning to help with type hinting (this tells the interpreter to always cast to
+    a field element of the same type). """
+
     def __post_init__(self) -> None:
         """Validates the FieldElement is in the proper field range"""
         if self.num >= self.prime or self.num < 0:
             raise ValueError(f"Num {self.num} not in field range 0 to {self.prime - 1}")
 
-    def __add__(self, other):
+    def __add__(self, other: "FieldElement") -> FE:
         """Performs field-addition on elements of the same field order."""
         if self.prime != other.prime:
             raise TypeError("Cannot add two numbers in different Fields")
         num = (self.num + other.num) % self.prime
-        return self.__class__(num, self.prime)
+        return cast(Type[FE], self.__class__(num, self.prime))
 
-    def __sub__(self, other):
+    def __sub__(self, other: "FieldElement") -> FE:
         """Performs field-subtraction on elements of the same field order."""
         if self.prime != other.prime:
             raise TypeError("Cannot subtract two numbers in different Fields")
         num = (self.num - other.num) % self.prime
-        return self.__class__(num, self.prime)
+        return cast(Type[FE], self.__class__(num, self.prime))
 
-    def __mul__(self, other):
+    def __mul__(self, other: "FieldElement") -> FE:
         """Performs field-multiplication on elements of the same field order."""
         if self.prime != other.prime:
             raise TypeError("Cannot multiply two numbers in different Fields")
         num = (self.num * other.num) % self.prime
-        return self.__class__(num, self.prime)
+        return cast(Type[FE], self.__class__(num, self.prime))
 
-    def __pow__(self, exponent):
-        """Performs field-exponentiation on elements of the same field order."""
-        num = pow(self.num, exponent, self.prime)
-        return self.__class__(num, self.prime)
+    def __pow__(self: "FieldElement", exponent: int) -> FE:
+        """Performs field-exponentiation on elements of a field order."""
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return cast(Type[FE], self.__class__(num, self.prime))
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: "FieldElement") -> FE:
         """Performs field-division on elements of the same field order."""
         if self.prime != other.prime:
             raise TypeError("Cannot divide two numbers in different Fields")
+        if other.num == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
-        return self.__class__(num, self.prime)
+        return cast(Type[FE], self.__class__(num, self.prime))
 
 
 def main():
