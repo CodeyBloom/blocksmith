2eb90ea6fb1a1869796185e93adeac1e89d9f430
Codey Bloom <codey.bloom@protonmail.com>
2025-05-15 04:51:00 +1000

    started implementing eliptic points

--- ecc.py
+++ ecc.py
@@ -1,23 +1,34 @@
+"""This module contains all the logic for doing elliptic curve cryptography (ECC) in python. This is basically my solutions
+to the problems and exercises in the first few chapters of Programming Bitcoin"""
+
 from dataclasses import dataclass
 from typing import Type, TypeVar, cast
 
-# Because I want type hints everywhere the FieldEleement dataclass will be self referential. This helps with that.
+# Because I want type hints everywhere the dataclasses will be self referential. This helps with that.
 FE = TypeVar("FE", bound="FieldElement")
 
 
 @dataclass(frozen=True)
 class FieldElement:
+    """A FieldElement is a member of a finite field of modulus prime."""
+
     num: int
     prime: int
 
-    """Below is all the requisite operations required to satisfy a field order. We use FE above to make it self-referring
+    """Below is all the requisite operations required to satisfy a finite field. We use FE above to make it self-referring
     with the type hints. We use cast when returning to help with type hinting (this tells the interpreter to always cast to
     a field element of the same type). """
 
-    def __post_init__(self) -> None:
+    def __post_init__(self: "FieldElement") -> None:
         """Validates the FieldElement is in the proper field range"""
         if self.num >= self.prime or self.num < 0:
             raise ValueError(f"Num {self.num} not in field range 0 to {self.prime - 1}")
+
+    def __ne__(self, other: "FieldElement") -> bool:
+        """Checks if two FieldElements are not equal."""
+        if other is None:
+            return False
+        return not self.num == other.num and self.prime == other.prime
 
     def __add__(self, other: "FieldElement") -> FE:
         """Performs field-addition on elements of the same field order."""
@@ -56,9 +67,20 @@ class FieldElement:
         return cast(Type[FE], self.__class__(num, self.prime))
 
 
-def main():
-    print("Hello from blocksmith!")
+@dataclass(frozen=True)
+class Point:
+    """A Point is a point in an elliptic curve. This will be populated with FielElements in ECC."""
 
+    a: FieldElement
+    b: FieldElement
+    x: FieldElement
+    y: FieldElement
 
-if __name__ == "__main__":
-    main()
+    def __post_init__(self: "Point") -> None:
+        """Validates the Point is on the curve."""
+        a = self.a
+        b = self.b
+        x = self.x
+        y = self.y
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError("({}, {}) is not on the curve".format(x, y))
